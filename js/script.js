b5c81897ef3402163f90d6ffd4b62684dafc9957
PawanJS <contactpawanjs@gmail.com>
2021-01-30 09:40:52 +0530

    some changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,1 @@
 "use strict";
-
-const paragraph = document.querySelectorAll(".js-paragraph");
-const btnOpenAccordion = document.querySelectorAll(".js-btn-open");
-const accordion = document.querySelectorAll(".js-accordion");
-
-const openAccordion = function () {
-  paragraph.forEach((p) => p.classList.add("paragraph-active"));
-};
-
-btnOpenAccordion.forEach((btn) => btn.addEventListener("click", openAccordion));
