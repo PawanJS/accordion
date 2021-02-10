437620c3fed106f5f24061731c81ef968752cf96
PawanJS <contactpawanjs@gmail.com>
2021-02-10 10:49:16 +0530

    Completed desktop design

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 "use strict";
 const btnAnswer = document.querySelectorAll(".js-btn");
+
+btnAnswer.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.parentElement.classList.contains("active")) {
+      btn.parentElement.classList.toggle("active");
+    } else {
+      btnAnswer.forEach((btn) => {
+        btn.parentElement.classList.remove("active");
+      });
+      btn.parentElement.classList.add("active");
+    }
+  });
+});
+
+// * The modern way
+const btnAnswerLi = document.querySelectorAll(".js-li");
