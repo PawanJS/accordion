46a02223437651ec34575d735520ba3d10d701a7
PawanJS <contactpawanjs@gmail.com>
2021-02-10 19:14:54 +0530

    Completed mobile design

--- js/script.js
+++ js/script.js
@@ -1,30 +1,14 @@
 "use strict";
 const btnAnswer = document.querySelectorAll(".js-btn");
+// * You can use parentElement as well
 
-btnAnswer.forEach((btn) => {
+btnAnswer.forEach((btn) =>
   btn.addEventListener("click", () => {
-    if (btn.parentElement.classList.contains("active")) {
-      btn.parentElement.classList.toggle("active");
+    if (btn.parentNode.classList.contains("active")) {
+      btn.parentNode.classList.toggle("active");
     } else {
-      btnAnswer.forEach((btn) => {
-        btn.parentElement.classList.remove("active");
-      });
-      btn.parentElement.classList.add("active");
+      btnAnswer.forEach((btn) => btn.parentNode.classList.remove("active"));
+      btn.parentNode.classList.add("active");
     }
-  });
-});
-
-// * The modern way
-// const btnAnswerLi = document.querySelectorAll(".js-li");
-
-// btnAnswerLi.addEventListener("click", function (e) {
-//   const btn = e.target.closet(".js-btn");
-//   if (btn.parentElement.classList.contains("active")) {
-//     btn.parentElement.classList.toggle("active");
-//   } else {
-//     btnAnswer.forEach((btn) => {
-//       btn.parentElement.classList.remove("active");
-//     });
-//     btn.parentElement.classList.add("active");
-//   }
-// });
+  })
+);
